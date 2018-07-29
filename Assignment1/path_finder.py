24c97287083e32a4ce13ef3d824f622fc2d08d30
Reme <31744912+PostalMist@users.noreply.github.com>
2018-07-29 05:51:55 -0400

    Update path_finder.py
    Heuristic done and path generation should be okay

--- Assignment1/path_finder.py
+++ Assignment1/path_finder.py
@@ -1,18 +1,30 @@
 import math
 
 class Node:
-    def __init__(self, x, y, value, f = 0, g = 0, h = 0, parent = 0):
+    def __init__(self, x, y, f = 0, g = 0, h = 0, parent = 0):
         self.x = x # row
         self.y = y # column
-        self.value = value # 1 for unblocked, 0 for blocked
         self.f = f # path cost + heuristic
         self.g = g # path cost
         self.h = h # heuristic
         self.parent = parent
-    def update_f():
-        self.f = self.g + self.h
-    def location():
+    def update_f(self, node):
+        self.f = node.f
+    def location(self):
         return self.x, self.y
+########################################################################################
+### global variables
+########################################################################################
+START_NODE = None
+GOAL_NODE = None
+WEIGHT = 1
+ZERO_HEURISTICS = 1
+numExpansions = 0
+numSteps = 0
+
+########################################################################################
+### functions
+########################################################################################
 
 def generateNeighbors(map, origin_node):
     """
@@ -25,13 +37,61 @@ def generateNeighbors(map, origin_node):
 
     neighbors = []
     x, y = origin_node.location()
+    if(x - 1 < 0):
+        pass
+    else:
+        if(map[x-1][y] == 1):#getting UP
+            neighbors.append(Node(x-1,y,1))
+    if(x + 1 >= len(map)):
+        pass
+    else:
+        if(map[x+1][y] == 1):# getting DOWN
+            neighbors.append(Node(x+1,y,1))
+           
+    if(y - 1 < 0):
+        pass
+    else:
+        if(map[x][y-1] == 1):#getting LEFT
+            neighbors.append(Node(x,y-1,1))
+    if(y + 1 >= len(map[x])):
+        pass
+    else:
+        if(map[x][y+1] == 1):# getting RIGHT
+            neighbors.append(Node(x,y+1,1))
+    return neighbors
+def h(origin_node, goal_node): 
+    """ function will compute the heuristic. It will compute the manhattan distance if zero_heuristic variable is one and
+    and the zero heuristic if zero is zero """
+    return ZERO_HEURISTIC * round( math.sqrt(((goal_node.x - origin_node.x)**2)+((goal_node.y - origin_node.y)**2)), 2)
 
-    if(x == 0): # not done
-        left = Node()
-        if()
-        neighbors.append(left)
-        neighbors.append(right)
-
+def g(origin_node, node):
+    """ returns cost from the origin node to any node """
+    return round( math.sqrt(((node.x - origin_node.x)**2)+((node.y - origin_node.y)**2)), 2)
+def min_f(queue):
+    """ finds the index of the node with the lowest f value in open_list """
+    min_value_index = 0
+    if(len(queue) == 1):
+        return 0
+    for i in range(1,len(queue)):
+        if(queue[min_value_index].f <= queue[i].f):
+            pass
+        else:
+            min_value_index = i
+    return min_value_index
+def path_build(node):
+    """ This function will iterate through a path through a list of its parents """
+    rPath = list()
+    pathCost = 0
+    ptr = node
+    while(ptr != None):
+        rPath.append((ptr.x,ptr.y))
+        pathCost = pathCost + ptr.g
+        ptr = ptr.parent
+    numSteps = len(rPath) - 1
+    return PathCost, rPath.reverse()
+        
+    
+        
 def PathFinder(map, initial_x, initial_y, goal_x, goal_y):
     """
         Args:
@@ -45,22 +105,35 @@ def PathFinder(map, initial_x, initial_y, goal_x, goal_y):
                 i.e. path from (0,0) -> (2,2): ((0,0), (0,1), (1,1), (1,2), (2,2))
     """
 
-    pathExists = True # boolean flag for whether or not path exists, assumed True until determined False
+    #pathExists = True # boolean flag for whether or not path exists, assumed True until determined False don't think we need this
 
     open_list = []
-    open_list.append(Node(initial_x, initial_y, map[initial_x][initial_y]))
+    path = []
+    pathCost = 0
+    GOAL_NODE = Node(goal_x,goal_y)
+    START_NODE = Node(initial_x, initial_y)
+    open_list.append(START_NODE)
     closed_list = []
+    neighbors = []
 
     while(len(open_list) != 0):
-        q = open_list[0]
-        for node in open_list: # determine lowest f score in open_list
-            if(node.f < q.f):
-                q = node
-            else:
-                continue
-        if(q.x == goal_x and q.y == goal_y):
-            closed_list.append((q.x, q.y))
-            return closed_list
-        else:
-            closed_list.append((q.x, q.y))
-            # create a generateNeighbors(Node) function
+        q = open_list.pop(min_f(open_list))
+        if(q.x == GOAL_NODE.x AND q.y == GOAL_NODE.y):
+            pathCost, path = path_build(GOAL_NODE)
+            return pathCost, path
+        if q not in closed_list:
+            neighbors = generateNeighbors(map, q)
+            for n in neighbors:
+                n.g = g(q,n)
+                n.h = h(q,GOAL_NODE)
+                n.f = n.g + (WEIGHT * n.h)
+                n.parent = q
+                if n not in open_list:
+                    open_list.append(n)
+                    numExpansions = numExpansions + 1
+                else:
+                    found_n = open_list[open_list.index(n)]
+                    if(n.f < found_n.f):
+                        found_n.update_f(n)
+       
+# create a generateNeighbors(Node) function
