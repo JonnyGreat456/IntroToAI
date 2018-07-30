ee0ac8ac9e22b9b6625aaac736a059a3f0207289
Reme <31744912+PostalMist@users.noreply.github.com>
2018-07-30 10:58:28 -0400

    Update path_finder.py

--- Assignment1/path_finder.py
+++ Assignment1/path_finder.py
@@ -1,4 +1,5 @@
 import math
+import argparse
 
 class Node:
     def __init__(self, x, y, f = 0, g = 0, h = 0, parent = 0):
@@ -42,27 +43,28 @@ def generateNeighbors(map, origin_node):
         pass
     else:
         if(map[x-1][y] == 1):#getting UP
-            neighbors.append(Node(x-1,y,1))
+            neighbors.append(Node(x-1,y))
     if(x + 1 >= len(map)):
         pass
     else:
         if(map[x+1][y] == 1):# getting DOWN
-            neighbors.append(Node(x+1,y,1))
+            neighbors.append(Node(x+1,y))
            
     if(y - 1 < 0):
         pass
     else:
         if(map[x][y-1] == 1):#getting LEFT
-            neighbors.append(Node(x,y-1,1))
+            neighbors.append(Node(x,y-1))
     if(y + 1 >= len(map[x])):
         pass
     else:
         if(map[x][y+1] == 1):# getting RIGHT
-            neighbors.append(Node(x,y+1,1))
+            neighbors.append(Node(x,y+1))
     return neighbors
 def h(origin_node, goal_node): 
     """ function will compute the heuristic. It will compute the manhattan distance if zero_heuristic variable is one and
     and the zero heuristic if zero is zero """
+    global ZERO_HEURISTIC
     #return ZERO_HEURISTIC * round( math.sqrt(((goal_node.x - origin_node.x)**2)+((goal_node.y - origin_node.y)**2)), 2)
     return ZERO_HEURISTIC * round((math.fabs(origin_node.x - goal_node.x) + math.fabs(origin_node.y - goal_node.y)),2)
 
@@ -89,6 +91,7 @@ def find(queue, targetNode):
     
 def path_build(node):
     """ This function will iterate through a path through a list of its parents """
+    global numSteps
     rPath = list()
     pathCost = 0
     ptr = node
@@ -116,11 +119,13 @@ def PathFinder(map, initial_x, initial_y, goal_x, goal_y):
 
     #pathExists = True # boolean flag for whether or not path exists, assumed True until determined False don't think we need this
 
+    global WEIGHT,START_NODE,GOAL_NODE,numExpansions
     open_list = []
     path = []
     pathCost = 0
-    GOAL_NODE = Node(goal_x,goal_y)
-    START_NODE = Node(initial_x, initial_y)
+    GOAL_NODE = Node(goal_x,goal_y,0,0)
+    START_NODE = Node(initial_x, initial_y,0,0)
+    START_NODE.h = h(START_NODE,GOAL_NODE)
     open_list.append(START_NODE)
     closed_list = []
     neighbors = []
@@ -134,7 +139,7 @@ def PathFinder(map, initial_x, initial_y, goal_x, goal_y):
         numExpansions = numExpansions + 1
         neighbors = generateNeighbors(map, q)
         for n in neighbors:
-            n.g = g(q,n)
+            n.g = q.g + g(q,n)
             n.h = h(n,GOAL_NODE)
             n.f = n.g + (WEIGHT * n.h)
             n.parent = q
@@ -148,5 +153,8 @@ def PathFinder(map, initial_x, initial_y, goal_x, goal_y):
                     found_n = open_list[find(open_list,n)]
                     if(n.f < found_n.f):
                         found_n.update_f(n)
+                        
+
+    
        
 # create a generateNeighbors(Node) function
