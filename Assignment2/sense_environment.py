c913ee58fb2087c743b47efb1c8a900fe07b78b7
Unknown <ganapatit@hotmail.com>
2018-08-11 21:49:59 -0400

    Yeah boi

--- Assignment2/sense_environment.py
+++ Assignment2/sense_environment.py
@@ -17,44 +17,64 @@ from PIL import Image
 #############################################################################
 #############################################################################
 #############################################################################
+class sensor:
+	def __init__(self):
+		self.rootPath = os.path.abspath(os.path.join(os.getcwd(), os.pardir))   # DO NOT EDIT
+		self.save_dir = self.rootPath + '/Model/tf_model'                       # DO NOT EDIT
+
+	def predict(self, input_x, model_version=0):
+		out = ""
+		if len(input_x) != (28*28):
+			print "The input image or input array is shaped incorrectly. Expecting a 28x28 image."
+		for i in xrange(0,28):
+			out = out+"\n"
+			for j in xrange(0,28):
+				if input_x[(i*28)+j]>0.5:
+					out = out + "1"
+				else:
+					out = out + "0"
+		print "Input image array: \n", out
+
+		os.chdir(self.rootPath)
+		print os.getcwd()
+		filename_x = "text.txt"
+		file_ = open(filename_x, "r")
+
+		filename = self.save_dir + '-' + str(model_version) + '.meta'           # DO NOT EDIT
+		filename2 = self.save_dir + '-' + str(model_version)                    # DO NOT EDIT
+		print('Opening Model: ' + str(filename))                                # DO NOT EDIT
+		saver = tf.train.import_meta_graph(filename)                            # DO NOT EDIT
+		sess = tf.InteractiveSession()                                          # DO NOT EDIT
+
+		os.chdir('.ros')
+
+		# INITIALIZE GLOBAL VARIABLES
+		sess.run(tf.global_variables_initializer())                             # DO NOT EDIT
+
+		# RESTORE MODEL LOSS OP AND INPUT PLACEHOLDERS
+		saver.restore(sess, filename2)                                          # DO NOT EDIT
+		graph = tf.get_default_graph()                                          # DO NOT EDIT
+
+
+		# accuracy = graph.get_tensor_by_name('op_accuracy:0')
+		x = graph.get_tensor_by_name('ph_x:0')
+		# y_ = graph.get_tensor_by_name('ph_y_:0')
+		y = graph.get_tensor_by_name('op_y:0')
+		output = tf.arg_max(y,1)
+
+		input_x = np.reshape(input_x, (-1, 784))
+
+		print('Classifying Image...')
+		output = sess.run(output, feed_dict={x: input_x})
+		sess.close()
+
+		print output
+
+		return output[0]
+
 def predict(input_x):
-	out = ""
-	if len(input_x) != (28*28):
-		print "The input image or input array is shaped incorrectly. Expecting a 28x28 image."
-	for i in xrange(0,28):
-		out = out+"\n"
-		for j in xrange(0,28):
-			if input_x[(i*28)+j]>0.5:
-				out = out + "1"
-			else:
-				out = out + "0"
-	print "Input image array: \n", out
-
-	filename = 'prx_core_ws/src/prx_core/sensing/tf_model-0.meta'
-        filename2 = 'prx_core_ws/src/prx_core/sensing/tf_model-0'
-        print('Opening Model: ' + str(filename))
-        saver = tf.train.import_meta_graph(filename)
-        sess = tf.InteractiveSession()
-
-        # INITIALIZE GLOBAL VARIABLES
-        sess.run(tf.global_variables_initializer())
-
-        # RESTORE MODEL LOSS OP AND INPUT PLACEHOLDERS
-        saver.restore(sess, filename2)
-        graph = tf.get_default_graph()
-
-
-        accuracy = graph.get_tensor_by_name('op_accuracy:0')
-        x = graph.get_tensor_by_name('ph_x:0')
-        y_ = graph.get_tensor_by_name('ph_y_:0')
-
-	y = graph.get_tensor_by_name('op_y')
-
-
-	prediction = sess.run(y_, feed_dict={x: input_x})
-	print prediction
-	# prediction = int(random.random()*9.9) #Current prediction is random
-	return prediction
+	predicter = sensor()
+	return predicter.predict(input_x)
 #############################################################################
 #############################################################################
 #############################################################################
